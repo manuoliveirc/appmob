47cf009677e573f382b4f23f1571685bc7621601
manuoliveirc <manuoliveiravga@gmail.com>
2024-03-07 10:36:38 -0300

    m

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, View } from "react-native"
+import { ImageBackground, Text, TextInput, View, Button } from "react-native"
 import { styles } from './styles'
 
 
@@ -6,8 +6,18 @@ export function Login() {
     const login = require('../../assets/background.png')
     return (
         <ImageBackground style={styles.container} source={login} >
-            <View style = {styles.SquareShapeView}> 
+            <View style={styles.box1} >
+                <Text style={styles.text}>Login</Text>
+                <View style={styles.email}>
+                    <Text style={styles.email}>E-mail:</Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <Text style={styles.senha}>Senha:</Text>
+                    <TextInput style={styles.input}></TextInput>
+                </View>
             </View>
         </ImageBackground >
     )
 }
+
+
+export default Login;
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,12 +1,38 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container:{
-        flex: 1
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    SquareShapeView: { 
-        width: 100, 
-        height: 100, 
-        backgroundColor: '#add8e6',
-      }
+    box1: {
+        width: 270,
+        height: 350,
+        backgroundColor: '#b0e0e6',
+        borderRadius: 15,
+    },
+    text: {
+        color: '#000',
+        fontSize: 24,
+        padding: 10,
+        margin: 8,
+    },
+    email: {
+        marginTop: 20,
+        margin: 8,
+        fontSize: 20,
+    },
+    senha: {
+        marginTop: 20,
+        margin: 8,
+        fontSize: 20,
+    },
+    input: {
+        backgroundColor: '#fff',
+        height: 40,
+        borderRadius: 30,
+    },
+    
+
 })
